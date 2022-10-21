0d50cc00fc13c2d7cdeff6d3a82a9899d4ccf0af
Tikhon <96545571+grinevtikhon@users.noreply.github.com>
2022-10-22 01:28:44 +0300

    Add files via upload

---
+++ gen_22_10/Generation.cpp
@@ -0,0 +1,56 @@
+#include "Generation.h"
+
+Generation::Generation(vector<int> _v, int _group_size, int _n, double _dispersion)
+{
+
+	group_size = _group_size;
+
+	dispersion = _dispersion;
+
+	for (int i = 0; i < _n * _group_size; ++i)
+		gen.push_back(_v);
+}
+
+void Generation::calculate_generation()
+{
+	for (int i = 0; i < gen.size(); ++i)
+	{
+		for (int j = 0; j < input.size(); ++j)
+		{
+			gen[i].calculate_mistake_for_dots(input, ans);
+		}
+	}
+
+	sort(gen.begin(), gen.begin());
+
+	return;
+}
+
+void Generation::set_input(vector<double> _v)
+{
+	input = _v;
+}
+
+vector<double> Generation::get_input()
+{
+	return input;
+}
+
+void Generation::set_ans(vector<double> _v)
+{
+	ans = _v;
+}
+
+vector<double> Generation::get_ans()
+{
+	return ans;
+}
+
+void Generation::generate_next_generation()
+{
+	int j = gen.size() - 1;
+	for (int i = 0; i < group_size; ++i)
+	{
+		gen[i].mutate(gen[j], dispersion);
+	}
+}
