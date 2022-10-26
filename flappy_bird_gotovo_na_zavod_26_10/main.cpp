dd9e7931d30d537b882e9f4201f8620a90c3f45b
Tikhon <96545571+grinevtikhon@users.noreply.github.com>
2022-10-26 22:12:20 +0300

    Add files via upload

---
+++ flappy_bird_gotovo_na_zavod_26_10/main.cpp
@@ -0,0 +1,134 @@
+// оно работало, птица без текстуры в виде шара
+
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <time.h>
+#include <string>
+#include <SFML/Graphics.hpp>
+#include <chrono>
+#include <thread>
+#include <SFML/System.hpp>
+#include <queue>
+
+#include "MyGraphic.h"
+#include "Network.h"
+#include "Generation.h"
+
+#include "Flappy_bird.h"
+#include "Pipe.h"
+#include "Barriers.h"
+
+
+using namespace std;
+
+//для меня
+const bool otladka = false;
+//////////////////////////
+const int Height = 720;
+const int Length = 1080;
+const int high_grass = 50;
+const int min_dist = 200;
+const double width_pipe = 100;
+const double horizontal_distance = 300;
+const double vertical_distance = 200;
+const double tick = 0.004f;
+const double g = 2400;
+
+
+// 1280 * 720
+
+Flappy_bird bird;
+
+Barriers enemy;
+
+void next_tick() {
+	bird.next_tick();
+	enemy.next_tick();
+
+	bird.check_bird_with_barriers(enemy);
+
+	return;
+}
+
+void restart() {
+	bird.reset();
+	enemy.reset();
+	enemy.start_pipes();
+
+	return;
+}
+
+int main()
+{
+	srand(time(NULL));
+
+	enemy.start_pipes();
+
+
+	sf::RenderWindow window(sf::VideoMode(Length, Height), "SFML works!");
+	sf::CircleShape circle(bird.r);
+
+	sf::RectangleShape rectangle;
+	circle.setFillColor(sf::Color::Yellow);
+	rectangle.setFillColor(sf::Color::Green);
+
+	//инвалидный переменные(костыли)
+	bool pr_space = false;
+	bool now_space = false;
+	bool pr_r = false;
+	bool now_r = false;
+
+	while (window.isOpen())
+	{
+		// считывание пробела + прыжок
+		now_space = sf::Keyboard::isKeyPressed(sf::Keyboard::Space);
+		if (now_space == true && pr_space == false) {
+			bird.jump();
+		}
+		pr_space = now_space;
+
+		now_r = sf::Keyboard::isKeyPressed(sf::Keyboard::R);
+		if (now_r == true && pr_r == false) {
+			restart();
+		}
+		pr_r = now_r;
+
+		next_tick();
+
+
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+
+		// рисуем тута
+		window.clear(sf::Color::Black);
+
+
+		enemy.draw_all(window);
+
+		bird.draw(window);
+
+		//for (int i = 0; i < enemy.pipes.size(); ++i)
+		//{
+		//	if (abs(enemy.pipes[i].first.x1 - enemy.pipes[i].second.x1) < 1) {
+		//		cout << "ALAARM !!!!!";
+		//	}
+		//	if (abs(enemy.pipes[i].first.x2 - enemy.pipes[i].second.x2) < 1) {
+		//		cout << "ALAARM &&&&&&&";
+		//	}
+		//	cout << enemy.pipes[i].first.x1 << " ";
+		//}
+
+		//cout << "SCORE:\t" << bird.score << endl;
+
+		window.display();
+	}
+
+	return 0;
+}
